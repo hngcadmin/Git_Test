5737efac0c65e7280d05ec67b846e031485ee186
Layne <Layne@qq.com>
2021-08-05 10:05:05 +0800

    first commit

---
+++ src/main/java/com/git/GitTest.java
@@ -0,0 +1,9 @@
+package com.git;
+
+import jdk.management.resource.internal.inst.SocketOutputStreamRMHooks;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
