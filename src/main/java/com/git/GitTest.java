5763a745d944f31719a399d7b6c02a9f1d4653eb
Layne <Layne@qq.com>
2021-08-05 10:05:33 +0800

    second commit

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -5,5 +5,6 @@ import jdk.management.resource.internal.inst.SocketOutputStreamRMHooks;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git1");
     }
 }
